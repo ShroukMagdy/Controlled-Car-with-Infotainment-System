5e18ca42035c024c4c97b7fa2915198ec972a9d3
Roanne-Ibrahim <74497857+Roanne-Ibrahim@users.noreply.github.com>
2021-06-25 21:41:18 +0200

    Add files via upload

---
+++ Infotainment_v2/build-Infotainement_Gui-Desktop-Debug/moc_mainwindow.cpp
@@ -0,0 +1,261 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Merging_3_Features/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[37];
+    char stringdata0[696];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 22), // "on_StartButton_clicked"
+QT_MOC_LITERAL(2, 34, 0), // ""
+QT_MOC_LITERAL(3, 35, 22), // "on_PauseButton_clicked"
+QT_MOC_LITERAL(4, 58, 21), // "on_NextButton_clicked"
+QT_MOC_LITERAL(5, 80, 21), // "on_PrevButton_clicked"
+QT_MOC_LITERAL(6, 102, 26), // "on_MuteRadioButton_clicked"
+QT_MOC_LITERAL(7, 129, 30), // "on_verticalSlider_valueChanged"
+QT_MOC_LITERAL(8, 160, 5), // "value"
+QT_MOC_LITERAL(9, 166, 14), // "Handle_Button0"
+QT_MOC_LITERAL(10, 181, 14), // "Handle_Button1"
+QT_MOC_LITERAL(11, 196, 14), // "Handle_Button2"
+QT_MOC_LITERAL(12, 211, 14), // "Handle_Button3"
+QT_MOC_LITERAL(13, 226, 14), // "Handle_Button4"
+QT_MOC_LITERAL(14, 241, 14), // "Handle_Button5"
+QT_MOC_LITERAL(15, 256, 14), // "Handle_Button6"
+QT_MOC_LITERAL(16, 271, 14), // "Handle_Button7"
+QT_MOC_LITERAL(17, 286, 14), // "Handle_Button8"
+QT_MOC_LITERAL(18, 301, 14), // "Handle_Button9"
+QT_MOC_LITERAL(19, 316, 15), // "Handle_Button10"
+QT_MOC_LITERAL(20, 332, 36), // "on_MonthComboBox_currentIndex..."
+QT_MOC_LITERAL(21, 369, 5), // "index"
+QT_MOC_LITERAL(22, 375, 11), // "ClockUpdate"
+QT_MOC_LITERAL(23, 387, 18), // "on_Date_OK_clicked"
+QT_MOC_LITERAL(24, 406, 20), // "on_VideoPlay_clicked"
+QT_MOC_LITERAL(25, 427, 20), // "on_NextVideo_clicked"
+QT_MOC_LITERAL(26, 448, 21), // "on_VideoPause_clicked"
+QT_MOC_LITERAL(27, 470, 20), // "on_VideoStop_clicked"
+QT_MOC_LITERAL(28, 491, 24), // "on_VideoPrevious_clicked"
+QT_MOC_LITERAL(29, 516, 26), // "on_MuteRadioButtom_clicked"
+QT_MOC_LITERAL(30, 543, 36), // "on_VerticalVolumeSlider_value..."
+QT_MOC_LITERAL(31, 580, 22), // "HomeW_MusicBtn_clicked"
+QT_MOC_LITERAL(32, 603, 25), // "HomeW_SettingsBtn_clicked"
+QT_MOC_LITERAL(33, 629, 22), // "HomeW_VideoBtn_clicked"
+QT_MOC_LITERAL(34, 652, 3), // "Esc"
+QT_MOC_LITERAL(35, 656, 26), // "on_HomeW_CameraBtn_clicked"
+QT_MOC_LITERAL(36, 683, 12) // "CameraUpdate"
+
+    },
+    "MainWindow\0on_StartButton_clicked\0\0"
+    "on_PauseButton_clicked\0on_NextButton_clicked\0"
+    "on_PrevButton_clicked\0on_MuteRadioButton_clicked\0"
+    "on_verticalSlider_valueChanged\0value\0"
+    "Handle_Button0\0Handle_Button1\0"
+    "Handle_Button2\0Handle_Button3\0"
+    "Handle_Button4\0Handle_Button5\0"
+    "Handle_Button6\0Handle_Button7\0"
+    "Handle_Button8\0Handle_Button9\0"
+    "Handle_Button10\0on_MonthComboBox_currentIndexChanged\0"
+    "index\0ClockUpdate\0on_Date_OK_clicked\0"
+    "on_VideoPlay_clicked\0on_NextVideo_clicked\0"
+    "on_VideoPause_clicked\0on_VideoStop_clicked\0"
+    "on_VideoPrevious_clicked\0"
+    "on_MuteRadioButtom_clicked\0"
+    "on_VerticalVolumeSlider_valueChanged\0"
+    "HomeW_MusicBtn_clicked\0HomeW_SettingsBtn_clicked\0"
+    "HomeW_VideoBtn_clicked\0Esc\0"
+    "on_HomeW_CameraBtn_clicked\0CameraUpdate"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      33,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  179,    2, 0x08 /* Private */,
+       3,    0,  180,    2, 0x08 /* Private */,
+       4,    0,  181,    2, 0x08 /* Private */,
+       5,    0,  182,    2, 0x08 /* Private */,
+       6,    0,  183,    2, 0x08 /* Private */,
+       7,    1,  184,    2, 0x08 /* Private */,
+       9,    0,  187,    2, 0x08 /* Private */,
+      10,    0,  188,    2, 0x08 /* Private */,
+      11,    0,  189,    2, 0x08 /* Private */,
+      12,    0,  190,    2, 0x08 /* Private */,
+      13,    0,  191,    2, 0x08 /* Private */,
+      14,    0,  192,    2, 0x08 /* Private */,
+      15,    0,  193,    2, 0x08 /* Private */,
+      16,    0,  194,    2, 0x08 /* Private */,
+      17,    0,  195,    2, 0x08 /* Private */,
+      18,    0,  196,    2, 0x08 /* Private */,
+      19,    0,  197,    2, 0x08 /* Private */,
+      20,    1,  198,    2, 0x08 /* Private */,
+      22,    0,  201,    2, 0x08 /* Private */,
+      23,    0,  202,    2, 0x08 /* Private */,
+      24,    0,  203,    2, 0x08 /* Private */,
+      25,    0,  204,    2, 0x08 /* Private */,
+      26,    0,  205,    2, 0x08 /* Private */,
+      27,    0,  206,    2, 0x08 /* Private */,
+      28,    0,  207,    2, 0x08 /* Private */,
+      29,    0,  208,    2, 0x08 /* Private */,
+      30,    1,  209,    2, 0x08 /* Private */,
+      31,    0,  212,    2, 0x08 /* Private */,
+      32,    0,  213,    2, 0x08 /* Private */,
+      33,    0,  214,    2, 0x08 /* Private */,
+      34,    0,  215,    2, 0x08 /* Private */,
+      35,    0,  216,    2, 0x08 /* Private */,
+      36,    0,  217,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   21,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_StartButton_clicked(); break;
+        case 1: _t->on_PauseButton_clicked(); break;
+        case 2: _t->on_NextButton_clicked(); break;
+        case 3: _t->on_PrevButton_clicked(); break;
+        case 4: _t->on_MuteRadioButton_clicked(); break;
+        case 5: _t->on_verticalSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->Handle_Button0(); break;
+        case 7: _t->Handle_Button1(); break;
+        case 8: _t->Handle_Button2(); break;
+        case 9: _t->Handle_Button3(); break;
+        case 10: _t->Handle_Button4(); break;
+        case 11: _t->Handle_Button5(); break;
+        case 12: _t->Handle_Button6(); break;
+        case 13: _t->Handle_Button7(); break;
+        case 14: _t->Handle_Button8(); break;
+        case 15: _t->Handle_Button9(); break;
+        case 16: _t->Handle_Button10(); break;
+        case 17: _t->on_MonthComboBox_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->ClockUpdate(); break;
+        case 19: _t->on_Date_OK_clicked(); break;
+        case 20: _t->on_VideoPlay_clicked(); break;
+        case 21: _t->on_NextVideo_clicked(); break;
+        case 22: _t->on_VideoPause_clicked(); break;
+        case 23: _t->on_VideoStop_clicked(); break;
+        case 24: _t->on_VideoPrevious_clicked(); break;
+        case 25: _t->on_MuteRadioButtom_clicked(); break;
+        case 26: _t->on_VerticalVolumeSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 27: _t->HomeW_MusicBtn_clicked(); break;
+        case 28: _t->HomeW_SettingsBtn_clicked(); break;
+        case 29: _t->HomeW_VideoBtn_clicked(); break;
+        case 30: _t->Esc(); break;
+        case 31: _t->on_HomeW_CameraBtn_clicked(); break;
+        case 32: _t->CameraUpdate(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 33)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 33;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 33)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 33;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
